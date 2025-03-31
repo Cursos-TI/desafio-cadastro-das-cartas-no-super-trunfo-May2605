10ca3478b0852198132db4c0527594445b717bd8
May2605 <mayaraas.mw@gmail.com>
2025-03-31 21:25:06 +0000

    cartas super trunfo 31/03

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,16 +6,7 @@ int main (){
     char estado[50];
 
     printf("Digite o numero do ponto turistico: \n");
-    scanf("%d" ,&"pontosturistico - população");
-
-    printf("Digite o pib: \n");
-    scanf("%f" ,&"pib");
-
-    printf("Digite o estado: \n");
-    scanf("%s" ,&"estado");
-
-    printf("numero do ponto turistico: %s","pontosturistico - população");
-    printf("pib: %d" - "estado" ": %f", "pib", "estado");
+    scanf("%d" 
 
         return 0;
 
---
+++ CartasSuperTrunfoDesafio.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int populacao, numerodepontosturisticos;
+    float area;
+    char Estado[8];
+
+printf("População: \n");
+scanf("%d", &populacao);
+
+printf("digite a Área: \n");
+scanf("%f", &area);
+
+printf("digite o estado: \n");
+scanf("%s", &Estado);
+
+printf("Digite pontos turisticos: \n");
+scanf("%d", &"pontosturisticos");
+
+printf("nome do população:%s - pontosturisticos: %d", populacao, numerodepontosturisticos);
+printf("área: %d - estado: %f", area, Estado);
+
+return 0;
+
+}
