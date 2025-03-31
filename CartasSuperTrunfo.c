0c0787afc53697f85303f41ed281e79cdd034b2f
May2605 <mayaraas.mw@gmail.com>
2025-03-31 14:53:01 +0000

    alteração hj

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 
-int main(){
-    char A [10];
+int main (){    
+    int pontoturistico, população;
+    float pib;
+    char estado[50];
+
+    printf("Digite o numero do ponto turistico: \n");
+    scanf("%d" ,&"pontosturistico - população");
+
+    printf("Digite o pib: \n");
+    scanf("%f" ,&"pib");
+
+    printf("Digite o estado: \n");
+    scanf("%s" ,&"estado");
+
+    printf("numero do ponto turistico: %s","pontosturistico - população");
+    printf("pib: %d" - "estado" ": %f", "pib", "estado");
+
+        return 0;
+
+
+
+
+
+
     
 
-    printf("Digite letra estado: \n");
-    scanf("%s", &A);
-
-    
-
-    
-         
 
 
 
-return 0;
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
