9f3d0dd82696d6e8035e694eafb22da9ceb17a94
May2605 <mayaraas.mw@gmail.com>
2025-04-01 19:20:35 +0000

    Concluído 01/04

--- CartasSuperTrunfoDesafio.c
+++ CartasSuperTrunfoDesafio.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 
 int main(){
+    printf("Carta 1!\n");
     int populacao, numerodepontosturisticos;
     float area, pib;
-    char estado[8], codigodacarta[A01, B02, C03], nomedacidade [50];
+    char estado[5], codigo[8], nomedacidade[50];
 
-printf("qual a população: \n");
+printf("Qual a População: \n");
 scanf("%d", &populacao);
 
-printf("digite a Área: \n");
+printf("Área: \n");
 scanf("%f", &area);
 
-printf("digite o estado: \n");
+printf("Numero Pontos Turisticos: \n");
+scanf("%d", &numerodepontosturisticos);
+
+printf("Estado: \n");
 scanf("%s", &estado);
 
-printf("quantos pontos turisticos \n");
-scanf("%d", &pontosturisticos);
+printf("Pib: \n");
+scanf("%f", &pib);
 
-printf("nome do populacao: %d - pontosturisticos: %d", populacao, pontosturisticos);
-printf("area: %f - estado: %s", area, estado);
+printf("Codigo: \n");
+scanf("%s", &codigo);
+
+printf("Nome da Cidade: \n");
+scanf("%s", &nomedacidade);
+
+
+
+printf("nome do populacao: %d - numero de pontos turisticos: %d\n", populacao, numerodepontosturisticos);
+printf("area: %f - estado: %s\n", area, estado);
+printf("pib: %f - codigo: %s\n", pib, codigo);
+printf("nomedacidade: %s\n", nomedacidade);
 
 return 0;
+
+
+
 
 }
